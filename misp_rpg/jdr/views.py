dc635c91e3e8de76bb099d759a4fde322e2735ea
cyberreaper <fb2501@free.fr>
2020-04-05 14:52:44 +0200

    first commit

---
+++ misp_rpg/jdr/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render
+from datetime import datetime
+from pymisp import PyMISP, MISPEvent
+from django.http import HttpResponse
+import pandas as pd
+
+# Create your views here.
+
+app_name='jdr/'
+def home(request):
+    """ Exemple de page non valide au niveau HTML pour que l'exemple soit concis """
+    return HttpResponse("""
+        <h1>Interface Eclipse phase</h1>
+        <p>La mort n'est qu'une péripétie</p>
+        <p>Des dangers autrement plus sérieux vous guettent</p>
+    """)
+
+def connect_misp():
+    misp_url = 'https://cyberchinois.eu'
+    misp_key = 'MXeOnxoTtNIf4MlzRVeOwCS2wnErwAGluj9umGYp'
+    misp = PyMISP(misp_url, misp_key)
+    return misp
+
+def MISP_list_event(requests):
+    misp=connect_misp()
+    r=misp.search(published=True)
+    listevents=[]
+    for event in r:
+        listevents.append([event['Event']['id'], event['Event']['info']])
+    return render(requests, app_name+'/misp.html', {'events':listevents})
+
+def view_event(requests, id_event):
+    misp=connect_misp()
+    r=misp.search(eventid=id_event)
+    info=r[0]['Event']['info']
+    attribute=[]
+    objects=[]
+    for z in r[0]['Event']['Attribute']:
+        attribute.append(z['value'])
+    try:
+        for obj in r[0]['Event']['Object']:
+            for attr in obj['Attribute']:
+                objects.append([attr['object_relation'], attr['value']])
+    except:
+        pass
+
+    return render(requests, app_name+'/display_event.html', {'event':info,
+                                                             'attributes':attribute,
+                                                             'objects':objects})
+
+def view_by_tags(requests, repors_tag):
+    misp = connect_misp()
+    s = misp.search(tags=repors_tag)
+    info=[]
+    attribute=[]
+    temp = []
+    final = []
+    for z in s:
+        info = z['Event']['info']
+        for attr in z['Event']['Attribute']:
+            temp.append(attr['value'])
+        final.append([info, temp[0]])
+        temp = []
+    return render(requests, app_name + '/display_reports.html', {'attributes': final})
